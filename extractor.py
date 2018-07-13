d0c170d83d02ea5c2f09bd1799fdb8eb76d4e3b4
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2018-07-13 14:02:25 -0700

    fixed prereq bug

--- extractor.py
+++ extractor.py
@@ -14,15 +14,18 @@ sem_map = {
         3: 'Fall',
         }
 
-
+#*******************************************************************************************************************
 def find_nth(haystack, needle, n):
     start = haystack.find(needle)
     while start >= 0 and n > 1:
         start = haystack.find(needle, start+len(needle))
         n -= 1
     return start
+#*******************************************************************************************************************
 
 
+
+#*******************************************************************************************************************
 def build_dict(url):
     # The page has a dangling /div
     # Python's html.parser doesn't handle this adequately, so we need html5lib
@@ -32,11 +35,23 @@ def build_dict(url):
     course_tables = soup.find_all(attrs={'class': 'class-list-course-list'})
     for course_table in course_tables:
         tickets = []
+
+        temp_prereq_str = course_table.find(attrs={'class': 'class-list-course-info'}).text.strip()
+        if (len(temp_prereq_str) > 12):
+            temp_prereq_split = temp_prereq_str.find('Prerequisite')
+            temp_prereq_str = temp_prereq_str[temp_prereq_split+13:].strip()
+        else:
+            temp_prereq_str = 'null'
+
+
+        course_info = {
+            'units' : course_table.find(attrs={'class': 'class-list-unit'}).text.strip()[7:],
+            'prereq': temp_prereq_str,
+            }
         course = {
                 'course_id': course_table.find(attrs={'class': 'course-id'}).text.strip(),
                 'course_title' : course_table.find(attrs={'class': 'class-list-course-title'}).text.strip(),
-                #FIXME split up Units and Prerequisites
-                'course_info' : course_table.find(attrs={'class': 'class-list-course-info'}).text.strip(),
+                'course_info' : course_info,
                 'course_description' : course_table.find(attrs={'class': 'class-list-course-desc'}).text.strip(),
                 'tickets': tickets,
                 }
@@ -46,47 +61,60 @@ def build_dict(url):
             temp_time_str = section.find(attrs={'title': 'TIME'}).text.strip()
             temp_day_str = section.find(attrs={'title': 'DAY'}).text.strip()
 
+            # First split is for getting the lec_day
+            # The second and third split will get the lab_day
             temp_day_str_split = temp_day_str.find('(')
+            temp_day_str_split2 = temp_day_str.find(')')
+            temp_day_str_split3 = find_nth(temp_day_str, '(', 2)
+
+            # This will split the two concatinated times into seperate values
             temp_time_str_split = find_nth(str(temp_time_str), ':',2)
 
             ticket = {
                     'number': section.find(attrs={'class': 'class-list-info-ticket'}).text.strip(),
                     'status': section.find(attrs={'class': 'class-list-info-status'}).text.strip(),
-                    #FIXME split up lecture/lab in the following three fields
-                    # instead of strings, these should be dicts with
-                    # {'lecture': ..., 'lab': ...} 
-                    #'day': str(section.find(attrs={'title': 'DAY'}).text.strip()),
+
                     'lec_day': str(temp_day_str)[:temp_day_str_split].strip(),
-                    'lab_day': str(temp_day_str)[temp_day_str_split+5:temp_day_str_split+10].strip(),
-                    #'time': section.find(attrs={'title': 'TIME'}).text.strip(),
+                    'lab_day': str(temp_day_str)[temp_day_str_split2+1:temp_day_str_split3].strip(),
 
                     # These splits will split the lecture and lab times into seperate vals
                     'lec_time': str(temp_time_str)[:temp_time_str_split+3],
                     'lab_time': str(temp_time_str)[temp_time_str_split+3:],
                     'room': section.find(attrs={'class': 'class-list-room-text'}).text.strip(),
+
                     # (string)[6:] is needed to strip the 'person' icon text out of the <span> tag
                     'instructor': (section.find(attrs={'title': 'INSTRUCTOR'}).text.strip())[6:],
                     }
             tickets.append(ticket)
     return courses
+#*******************************************************************************************************************
 
 
 
+#*******************************************************************************************************************
 def ticket_list(courses):
     tickets = []
     for course in courses:
         for ticket in course['tickets']:
             tickets.append(ticket['number'])
     return tickets
+#*******************************************************************************************************************
 
+
+
+#*******************************************************************************************************************
 if __name__ == '__main__':
     courses = build_dict(url.format(year=year, semester=semester))
     import json
+
+    # templateized so that the year and semester can easily be changed with globals
     cFileName = 'courses{year}-{semester}.json'
     with open(cFileName.format(year=year, semester=semester), 'w') as file:
         json.dump(courses, file, sort_keys=True, indent=4)
+
+    # templateized so that the year and semester can easily be changed with globals
     tickets = ticket_list(courses)
     tFilename = 'tickets{year}-{semester}.txt'
     with open(tFilename.format(year=year, semester=semester), 'w') as file:
         file.write('\n'.join(tickets))
-
+#*******************************************************************************************************************

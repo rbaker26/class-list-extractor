ac11bc8cef3398ef4b6ddb8532df82a398547c5b
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2018-07-12 23:56:57 -0700

    cleaned-up, but still bug with the times

--- extractor.py
+++ extractor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from bs4 import BeautifulSoup
+from gettext import find
 from urllib.request import urlopen
 
 # templateized so that the year and semester can easily be changed
@@ -12,6 +13,13 @@ sem_map = {
         2: 'Summer',
         3: 'Fall',
         }
+
+#def space_stripper(string):
+#    if (str(string).endswith(' ')):
+#        string = str(string).[:len(string)-1]
+#        space_stripper(string)
+#    elif (str(string).strip)
+
 
 def build_dict(url):
     # The page has a dangling /div
@@ -32,18 +40,30 @@ def build_dict(url):
                 }
         courses.append(course)
         for section in course_table.find_all(attrs={'class': 'class-list-info-method'}):
+            # Temp strings to parse out the different times and days for lecture and lab
+            temp_time_str = section.find(attrs={'title': 'TIME'}).text.strip()
+            temp_day_str = section.find(attrs={'title': 'DAY'}).text.strip()
+            temp_day_str_split = temp_day_str.find('(')
+
             ticket = {
                     'number': section.find(attrs={'class': 'class-list-info-ticket'}).text.strip(),
                     'status': section.find(attrs={'class': 'class-list-info-status'}).text.strip(),
                     #FIXME split up lecture/lab in the following three fields
                     # instead of strings, these should be dicts with
                     # {'lecture': ..., 'lab': ...} 
-                    'day': section.find(attrs={'title': 'DAY'}).text.strip(),
-                    'time': section.find(attrs={'title': 'TIME'}).text.strip(),
+                    #'day': str(section.find(attrs={'title': 'DAY'}).text.strip()),
+                    'lec_day': str(temp_day_str)[:temp_day_str_split-1].strip(),
+                    'lab_day': str(temp_day_str)[temp_day_str_split+5:temp_day_str_split+9].strip(),
+                    #'time': section.find(attrs={'title': 'TIME'}).text.strip(),
+                    #11:00AM - 12:1512:30PM - 1:20
+                    'lec_time': str(temp_time_str)[0:13],
+                    'lab_time': str(temp_time_str)[13:],
                     'room': section.find(attrs={'class': 'class-list-room-text'}).text.strip(),
+                    # (string)[6:] is needed to strip the 'person' icon text out of the <span> tag
                     'instructor': (section.find(attrs={'title': 'INSTRUCTOR'}).text.strip())[6:],
                     }
             tickets.append(ticket)
+            
 
     return courses
 
